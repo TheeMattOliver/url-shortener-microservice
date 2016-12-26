96eabd67896b7e764d8f0648083708d3f12f5f68
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-26 10:43:08 -0600

    setting up db env config

---
+++ env/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbUrl: 'mongodb://url-shorter-admin:Computeraspectofcyber!@ds143608.mlab.com:43608/url-shortener'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var router = express.Router();
+
+
 var mongodb = require('mongodb');
+if(!process.env.dbUrl) {
+
+var mlab = require('../env/config.js')
+  } else {
+  var mlab = 'https://shortenurlpls.herokuapp.com/:${process.env.PORT}/dbUrl'  
+}
+
+var MongoClient = mongodb.MongoClient;
+
 var shortid = require('shortid');
 var validUrl = require('valid-url');
-var router = express.Router();
+
+var port = process.env.PORT || 3000;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,9 +23,32 @@ router.get('/', function(req, res, next) {
 });
 
 // :url(*) allows us to pass in properly formatted links
-router.get('/new/:url(*)', function(req, res, next) {
-  console.log("trying out the get request:", req.params.url)
-  res.send(req.params.url);
+router.get('/new/:url(*)', function(req, res, next) {  
+  
+  MongoClient.connect(mLab, function(err, db) {
+      if (err) {
+        console.log("Unable to connect to the server", err);
+      } else {
+        console.log("Connected to server")
+        var collection = db.collection('links');
+        var params = req.params.url;
+      };
+  }); 
+
+  var newLink = function(db, callback) {
+    var insertLink = { url: params, short: "test" };
+    collection.insert([insertLink]);
+    res.send(params);
+  };
+
+  newLink(db, function() {
+    db.close();
+  });
+
+  
 });
+
+
+
 
 module.exports = router;
