c21507ec5df34a11e50bd9b25bc099c730b077a7
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-26 09:04:55 -0600

    starting over from scratch

--- server/env/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  dbUrl: 'mongodb://url-shorter-admin:Computeraspectofcyber!@ds143608.mlab.com:43608/url-shortener'
-}
-
-
-// example of how to set heroku config vars
-// heroku config:set MONGOLAB_URI=mongodb://url-shorter-admin:Computeraspectofcyber!@ds143608.mlab.com:43608/url-shortener
