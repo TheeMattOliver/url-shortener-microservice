fd5246806dc6e8da6a1e0ac640c1d085d16e2bc1
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-26 08:51:02 -0600

    changing tests

--- test/create_test.js
+++ test/create_test.js
@@ -4,7 +4,6 @@ const User = require('../server/db/link.js');
 describe('Creating records', () => {
   it('saves a link', (done) => {
     const google = new Link({ originalUrl: 'https://www.google.com' });
- 
     google.save()
      .then(() => {
        // Has google been saved successfully?
