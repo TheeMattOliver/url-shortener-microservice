b5f0e20159557f038b786336ce76e77c5fb825bc
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-26 10:10:14 -0600

    adding url route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var shortid = require('shortid');
+var validUrl = require('valid-url');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// :url(*) allows us to pass in properly formatted links
+router.get('/new/:url(*)', function(req, res, next) {
+  console.log("trying out the get request:", req.params.url)
+  res.send(req.params.url);
 });
 
 module.exports = router;
