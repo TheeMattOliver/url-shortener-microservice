8e497cdaf51a9e601729f89f5cb28fd712b25d9b
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-12-28 10:34:09 -0600

    filtering for xss on ejs calls

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('secure-filters').configure(require('ejs'));
 
 var MongoClient = require('mongodb').MongoClient
 
