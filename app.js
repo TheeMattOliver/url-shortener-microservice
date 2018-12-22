0eb4073d3ea086596cfefd0c773ab7983bdeca5d
Matt Oliver <mattoliver.mattoliver@gmail.com>
2018-12-22 09:02:25 -0600

    maintenance

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var MongoClient = require('mongodb').MongoClient
 var index = require('./routes/index');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
